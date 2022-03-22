4850e921ba2a94c3539bc0ceebc7cb94c4eb1098
Rimba Erlangga <rimbaerlangga@gmail.com>
2022-03-22 14:13:43 +0700

    fix result format

--- kateglo-scrapper.py
+++ kateglo-scrapper.py
@@ -6,22 +6,26 @@ import json
 
 driver = wd.Chrome(service=Service(ChromeDriverManager().install()))
 
+def to_text(element):
+    return element.text
+
 def scrap(p):
     url = f'https://kateglo.com/?&mod=glossary&op=1&phrase=&dc=teknologiinformasi&lang=&src=&srch=Cari&p={p}'
     driver.get(url)
 
     rows = driver.find_elements(By.XPATH, value='//table/tbody/tr/td[1]')
-    english_terms = driver.find_elements(By.XPATH, value='//table/tbody/tr/td[2]')
-    indonesian_terms = driver.find_elements(By.XPATH, value='//table/tbody/tr/td[3]')
-    keywords = driver.find_elements(By.XPATH, value='//table/tbody/tr/td[4]')
+    english_terms = map(to_text, driver.find_elements(By.XPATH, value='//table/tbody/tr/td[2]'))
+    indonesian_terms = map(to_text, driver.find_elements(By.XPATH, value='//table/tbody/tr/td[3]'))
+    keywords = map(to_text, driver.find_elements(By.XPATH, value='//table/tbody/tr/td[4]'))
 
-    return {
-        'p': p,
-        'rows': rows,
-        'en': english_terms,
-        'id': indonesian_terms,
-        'keyword': keywords
-    }
+    result = [{
+        'p': p
+        'en': ent,
+        'id': idt,
+        'keyword': kw
+    } for ent, idt, kw in zip(english_terms, indonesian_terms, keywords)]
+
+    return result
 
 def main():
     ret = {
@@ -29,7 +33,7 @@ def main():
     }
 
     for p in range(1, 66):
-        ret['result'].append(scrap(p))
+        ret['result'].extend(scrap(p))
 
     with open('result.json', 'w') as f:
         f.write(json.dumps(ret, indent=4))
